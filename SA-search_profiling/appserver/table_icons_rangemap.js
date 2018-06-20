0e1ee4bf8b1dd02e7af1a030424a895b3d061eba
Gary Burgett <gburgett@splunk.com>
2018-06-20 13:35:20 -0700

    Adding additional usecase content

---
+++ SA-search_profiling/appserver/table_icons_rangemap.js
@@ -0,0 +1,40 @@
+require([
+    'underscore',
+    'jquery',
+    'splunkjs/mvc',
+    'splunkjs/mvc/tableview',
+    'splunkjs/mvc/simplexml/ready!'
+], function(_, $, mvc, TableView) {
+
+    // Translations from rangemap results to CSS class
+    var ICONS = {
+        severe: 'alert-circle',
+        elevated: 'alert',
+        low: 'check-circle'
+    };
+
+    var RangeMapIconRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            // Only use the cell renderer for the range field
+            return cell.field === 'use_cases' || cell.field === 'title';
+        },
+        render: function($td, cell) {
+            var icon = 'question';
+            // Fetch the icon for the value
+            if (ICONS.hasOwnProperty(cell.value)) {
+                icon = ICONS[cell.value];
+            }
+            // Create the icon element and add it to the table cell
+            $td.addClass('icon').html(_.template('<i class="icon-<%-icon%> <%- range %>" title="<%- range %>"></i>', {
+                icon: icon,
+                range: cell.value
+            }));
+        }
+    });
+
+    mvc.Components.get('table1').getVisualization(function(tableView){
+        // Register custom cell renderer, the table will re-render automatically
+        tableView.addCellRenderer(new RangeMapIconRenderer());
+    });
+
+});
